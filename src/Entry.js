10fc8f870610200f98919c9a6c5834ef5754400b
3A-2Z <azizamino12@gmail.com>
2020-12-25 16:53:33 -0500

    fixed url path

--- src/Entry.js
+++ src/Entry.js
@@ -9,7 +9,7 @@ import { wait, waitFor } from '@testing-library/react';
 */
 function Entry(){
     
-    let currPath = window.location.pathname.split("/")
+    let currPath = window.location.pathname.replace("/#",'').split("/")
     
     let entryID = " "
     entryID = (currPath.length === 3)? currPath[2] : currPath[1] // Parses the URL for an journal entry ID to be used in the fetching of any changes to that entry
